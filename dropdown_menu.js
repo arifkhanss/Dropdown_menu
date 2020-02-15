1a5165a807c9ffc755c8279f85b5471dbe513468
Arif <60794183+arifkhanss@users.noreply.github.com>
2020-02-16 04:01:31 +0600

    Add files via upload

---
+++ dropdown_menu.js
@@ -0,0 +1,14 @@
+ var nav=document.querySelector(".container");
+  var oprnnav=document.querySelector(".openav");
+   var closenav=document.querySelector(".closeNav");
+
+   oprnnav.addEventListener("click",() =>{
+nav.style.left="0px"
+
+});
+
+
+closenav.addEventListener("click",() =>{
+nav.style.left="-250px"
+
+});
